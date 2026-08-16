e84e5f9a69932a90b7d541060df7b5b9f711052a
angieyapwt <angieyapwt@gmail.com>
2026-08-16 16:45:07 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "handover360-v26";
-const ASSETS = ["./", "./index.html", "./styles.css?v=26", "./app.js?v=26", "./manifest.json", "./og.png", "./vendor/pdf-lib.min.js"];
+const CACHE = "handover360-v28";
+const ASSETS = ["./", "./index.html", "./styles.css?v=28", "./app.js?v=28", "./manifest.json", "./og.png", "./vendor/pdf-lib.min.js"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
